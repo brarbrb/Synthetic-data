e464a3b61ea11b8cfe05a00a6979676880754ac8
Barbara <alexandrovabrbr@gmail.com>
2025-08-02 20:58:40 +0300

    ground truth for boxes corners as markers (for now)

--- examples/render_and_paste/obj_features.py
+++ examples/render_and_paste/obj_features.py
@@ -7,23 +7,24 @@ import os
 obj_folder = "/datashare/project/surgical_tools_models"
 
 bproc.init()
-IDS = {"needle_holder" : 1, "tweezers" : 2} # TODO: make sure it aligns with Gal's part 2
-features_set_NH = set()
-features_set_Tweezers = set()
+CATEGORY_IDS  = {"needle_holder" : 1, "tweezers" : 2} # TODO: make sure it aligns with Gal's part 2
+material_features = {
+    "needle_holder": set(),
+    "tweezers": set()
+}
 
-
-for subfolder in os.listdir(obj_folder):
-    full_path = os.path.join(obj_folder, subfolder)
+for category in os.listdir(obj_folder):
+    full_path = os.path.join(obj_folder, category)
     if os.path.isdir(full_path):
-        print(f"\n=== Entering category '{subfolder}' ===")
+        print(f"=== Entering category '{category}' ===")
 
         for obj_name in os.listdir(full_path):
             if obj_name.endswith('.obj'):
-                print(f"\n----- Loading: {obj_name} -----")
+                print(f"----- Loading: {obj_name} -----")
                 obj_path = os.path.join(full_path, obj_name)
 
                 obj = bproc.loader.load_obj(obj_path)[0]
-                obj.set_cp("category_id", IDS[subfolder])
+                obj.set_cp("category_id", CATEGORY_IDS [category])
 
                 # overall analysis of the object
                 print(f"Object name: {obj.get_name()}")
@@ -33,6 +34,7 @@ for subfolder in os.listdir(obj_folder):
                 print(f"Object scale: {obj.get_scale()}")
                 print(f"Object location: {obj.get_location()}")
                 print(f"Object rotation: {obj.get_rotation_euler()}")
+                print(f"Object local to world matrix: {obj.get_local2world_mat()}")
 
                 # Material analysis
                 materials = obj.get_materials()
@@ -47,13 +49,19 @@ for subfolder in os.listdir(obj_folder):
                             bsdf_node = node
                             break
                     if bsdf_node:
-                        # print("Material Inputs:")
+                        print("Material Inputs:")
                         for inp in bsdf_node.inputs:
-                            if subfolder == "needle_holder":
-                                features_set_NH.add(inp.name)
-                            else:
-                                features_set_Tweezers.add(inp.name)
+                            if inp.name not in material_features[category]:
+                                print(f"{inp.name} found first in object {obj_name}")
+                            material_features[category].add(inp.name)
                             # print(f"- {inp.name}")
-print("Materilals for needle holder: ", features_set_NH)
-print("materials for Tweezers: ", features_set_Tweezers)
-                    
+
+
+print("=== Summary of Material Inputs ===")
+for category, features in material_features.items():
+    print(f"{category.capitalize()}: {sorted(features)}")
+    
+if material_features['needle_holder'] == material_features['tweezers']:
+    print("The objects have identical blender properties!")
+else: 
+    print("The ojects have some different properties")
